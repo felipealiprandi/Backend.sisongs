bd6c907b79ef9387b21d18cc71b2302059127cf5
Felipe Aliprandi <aliprandi86@gmail.com>
2023-05-28 21:45:08 -0300

    b

---
+++ Aplication/Controllers/OngsController.cs
@@ -0,0 +1,34 @@
+﻿using Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+using Sisongs.Application.Services;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MeuProjeto.WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class OngsController : ControllerBase
+    {
+        private readonly IOngService _ongService;
+
+        public OngsController(IOngService ongService)
+        {
+            _ongService = ongService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Ong>>> Get()
+        {
+            var ongs = await _ongService.GetAllOngsAsync();
+            return Ok(ongs);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Ong ong)
+        {
+            await _ongService.CreateOngAsync(ong);
+            return Ok(ong);
+        }
+    }
+}
---
+++ Aplication/Controllers/ProjetosController.cs
@@ -0,0 +1,27 @@
+﻿using Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+using Sisongs.Application.Services;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MeuProjeto.WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProjetosController : ControllerBase
+    {
+        private readonly IProjetoService _projetoService;
+
+        public ProjetosController(IProjetoService projetoService)
+        {
+            _projetoService = projetoService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Projeto>>> Get()
+        {
+            var projetos = await _projetoService.GetAllProjetosAsync();
+            return Ok(projetos);
+        }
+    }
+}
