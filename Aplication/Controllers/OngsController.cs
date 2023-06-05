37a2d7a40fbbccc470a210c790776c1657781650
Felipe Aliprandi <aliprandi86@gmail.com>
2023-06-05 10:21:42 -0300

    f

--- Aplication/Controllers/OngsController.cs
+++ Aplication/Controllers/OngsController.cs
@@ -15,41 +15,11 @@ namespace API.Controllers
             _ongService = ongService;
         }
 
-        //[HttpGet]
-        //public async Task<ActionResult<List<Ong>>> Get()
-        //{
-        //    var ongs = await _ongService.GetAllOngsAsync();
-        //    return Ok(ongs);
-        //}
-
         [HttpGet]
-        public string Get()
+        public async Task<ActionResult<List<Ong>>> Get()
         {
-            try
-            {
-                var ongs =  _ongService.GetAllOngsAsync();
-            }
-            catch (Exception ex)
-            {
-
-                return ex.Message;
-            }
-
-            return "deu certo";
-        }
-
-        [HttpGet("novo")]
-        public async Task<string> Get2()
-        {
-            try
-            {
-                return "deu certo";
-            }
-            catch (Exception ex)
-            {
-
-                return ex.Message;
-            }
+            var ongs = await _ongService.GetAllOngsAsync();
+            return Ok(ongs);
         }
 
         [HttpPost]
